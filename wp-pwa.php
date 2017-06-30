b94d6b0d4983a6fefd1a0c4d6301a6bd912a3874
miya0001 <miya@wpist.me>
2017-06-30 18:09:26 +0900

    first commit

---
+++ wp-pwa.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wp Pwa
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-pwa
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Pwa
+ */
